b23348c7ec2f8541c397a80b513b4461abf2328c
Ravindu <164388768+RavinduPe@users.noreply.github.com>
2024-10-28 22:37:28 +0530

    Bug fixed

--- admin/studentAdmin/bulk_upload.php
+++ admin/studentAdmin/bulk_upload.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['role'])) {
     exit();
 }
 
-include($_SERVER['DOCUMENT_ROOT'] . '/config/connect.php');
-require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/ERS-Web_Technologies/config/connect.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/ERS-Web_Technologies/vendor/autoload.php');
 
 if (isset($_POST['upload'])) {
     $email = $_POST['email'];
